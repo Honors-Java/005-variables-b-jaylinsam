74c009e3f4598d6124811c51a6274aaf639b2123
Jaylin Bratcher-McEachern <651608@aacps.org>
2020-10-21 13:39:42 +0000

    I fished my variable B randomness assignment

--- index.java
+++ index.java
@@ -1,9 +1,16 @@
+int xPosition = 100;
+int yPosition = 100;
+int rectWidth = 200;
+int rectHeight = 200;
+int r = 150;
+int g = 0;
+int b = 0;
+
 void setup() {
 	size(500, 500);
+  frameRate(30);
 }
-int x = 250;
-int y = 200; 
-int d = 100; 
+
 void draw() {
 // Step 1: Write code that draws the following screenshots 
 // with hard-coded values. (Feel free to use colors 
@@ -18,15 +25,26 @@ void draw() {
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
 
-  background(175);
+  //background(255);
   stroke(0);
-fill(255);
 
-ellipse(x, y, d, d)
-x--;
-y--;
+  fill(r, g, b);
+  rect(xPosition, yPosition, rectWidth, rectHeight);
+  fill((int) random(0, 255), (int) random(0, 255), (int) random(0, 255));
+  ellipse(mouseX + (int) random(-30, 30), mouseY + (int) random(-30, 30), 10, 10);
+
+
+
 }
 
-void mouseprssed(){
-x = x-1
+void mousePressed(){
+  r = (int) random(0, 255);
+  g = (int) random(0, 255);
+  b = (int) random(0, 255);
+  background(255);
+  xposition = (int) random(0, 400);
+  yposiiton = (int) random(0, 400);
+  rectWidth = (int) random(50, 400);
+  rectHeight = (int) random(50, 400);
+
 }
